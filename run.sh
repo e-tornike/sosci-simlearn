d570a699b30ec806aebb720eb54958e26d5bccb6
e-tornike <20404466+e-tornike@users.noreply.github.com>
2023-12-21 11:00:07 +0100

    add run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+export _TYPER_STANDARD_TRACEBACK=1
+
+train_path="/home/tornike/Coding/phd/sosci-simlearn/data/meta_pairs_20231221-011820_train.jsonl"
+val_path="/home/tornike/Coding/phd/sosci-simlearn/data/meta_pairs_20231221-011820_val.jsonl"
+output_dir="/home/tornike/Coding/phd/sosci-simlearn/results"
+epochs=50
+
+# models="sentence-transformers/distiluse-base-multilingual-cased-v1 sentence-transformers/distiluse-base-multilingual-cased-v2 sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 sentence-transformers/paraphrase-multilingual-mpnet-base-v2 intfloat/multilingual-e5-base intfloat/e5-base-v2 thenlper/gte-base T-Systems-onsite/cross-en-de-roberta-sentence-transformer bert-base-multilingual-uncased"
+models="intfloat/multilingual-e5-base intfloat/e5-base-v2 thenlper/gte-base T-Systems-onsite/cross-en-de-roberta-sentence-transformer bert-base-multilingual-uncased"
+models="intfloat/multilingual-e5-base"
+for model in $models; do
+    python sosci_simlearn/train.py --train-path=$train_path --val-path=$val_path --output-dir=$output_dir --max-epochs=$epochs --model-name=$model
+done
