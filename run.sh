b95dea953d1f9ac7baa08eaa9782343711c18655
e-tornike <20404466+e-tornike@users.noreply.github.com>
2024-01-14 18:24:21 +0100

    update script

--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@ export _TYPER_STANDARD_TRACEBACK=1
 
 echo $CUDA_VISIBLE_DEVICES
 
+timestamp=$(date +'%m-%d-%Y_%H-%M')
+
 artifact_dir="/home/tornike/Coding/phd/sosci-simlearn/"
 
-train_path=$artifact_dir"data/meta_pairs_20231221-011820_train.jsonl"
-val_path=$artifact_dir"data/meta_pairs_20231221-011820_val.jsonl"
-output_dir=$artifact_dir"results"
+train_path=$artifact_dir"data/filtered_meta_pairs_20240113-122949_train.jsonl"
+val_path=$artifact_dir"data/filtered_meta_pairs_20240113-122949_val.jsonl"
+output_dir=$artifact_dir"results/filtered/"$timestamp
 epochs=50
 
 models="sentence-transformers/distiluse-base-multilingual-cased-v1 sentence-transformers/distiluse-base-multilingual-cased-v2 sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 sentence-transformers/paraphrase-multilingual-mpnet-base-v2 intfloat/multilingual-e5-base intfloat/e5-base-v2 thenlper/gte-base T-Systems-onsite/cross-en-de-roberta-sentence-transformer bert-base-multilingual-uncased"
